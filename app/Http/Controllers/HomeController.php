ad3c1ea2550d0528e3945cc51dc5c527c6423af1
arai motoki <arai@nal.vn>
2021-04-27 11:49:38 +0900

    providerの前まで2:10:00

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Memo;
+use App\Tag;
 
 class HomeController extends Controller
 {
@@ -24,8 +25,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-
-        return view('home');
+        $user = \Auth::user();
+        $memos = Memo::where('user_id', $user['id'])->where('status', 1)->orderBy('updated_at', 'DESC')->get();
+        return view('create', compact('user', 'memos'));
     }
     /**
      * Show the application dashboard.
@@ -35,7 +37,8 @@ class HomeController extends Controller
     public function create()
     {
         $user = \Auth::user();
-        return view('create', compact('user'));
+        $memos = Memo::where('user_id', $user['id'])->where('status', 1)->orderBy('updated_at', 'DESC')->get();
+        return view('create', compact('user', 'memos'));
     }
     /**
      * Show the application dashboard.
@@ -45,28 +48,52 @@ class HomeController extends Controller
     public function store(Request $request)
     {
         $data = $request->all();
-        // dd($data);
         // POSTされたデータをDB（memosテーブル）に挿入
         // MEMOモデルにDBへ保存する命令を出す
 
-        // 同じタグがあるか確認
-        // $exist_tag = Tag::where('name', $data['tag'])->where('user_id', $data['user_id'])->first();
-        // if (empty($exist_tag['id'])) {
-        //     //先にタグをインサート
-        //     $tag_id = Tag::insertGetId(['name' => $data['tag'], 'user_id' => $data['user_id']]);
-        // } else {
-        //     $tag_id = $exist_tag['id'];
-        // }
+        $exist_tag = Tag::where('name', $data['tag'])->where('user_id', $data['user_id'])->first();
+        if (empty($exist_tag['id'])) {
+            //先にタグをインサート
+            $tag_id = Tag::insertGetId(['name' => $data['tag'], 'user_id' => $data['user_id']]);
+        } else {
+            $tag_id = $exist_tag['id'];
+        }
         //タグのIDが判明する
         // タグIDをmemosテーブルに入れてあげる
         $memo_id = Memo::insertGetId([
             'content' => $data['content'],
             'user_id' => $data['user_id'],
-            // 'tag_id' => $tag_id,
+            'tag_id' => $tag_id,
             'status' => 1
         ]);
 
         // リダイレクト処理
         return redirect()->route('home');
+    }
+    public function edit($id)
+    {
+        // 該当するIDのメモをデータベースから取得
+        $user = \Auth::user();
+        $memo = Memo::where('status', 1)->where('id', $id)->where('user_id', $user['id'])
+            ->first();
+        $memos = Memo::where('user_id', $user['id'])->where('status', 1)->orderBy('updated_at', 'DESC')->get();
+        // dd($memo);s
+        //取得したメモをViewに渡す
+        $tags = Tag::where('user_id', $user['id'])->get();
+        return view('edit', compact('memo', 'user', 'memos', 'tags'));
+    }
+    public function update(Request $request, $id)
+    {
+        $inputs = $request->all();
+        // dd($inputs);
+        Memo::where('id', $id)->update(['content' => $inputs['content'], 'tag_id' => $inputs['tag_id']]);
+        return redirect()->route('home');
+    }
+    public function delete(Request $request, $id)
+    {
+        $inputs = $request->all();
+        // dd($inputs);
+        Memo::where('id', $id)->update(['status' => 2]);
+        return redirect()->route('home')->with('success', 'メモの削除ができました。');
     }
 }
--- database/migrations/2021_04_26_154751_add_tag_id_to_memos_table.php
+++ database/migrations/2021_04_26_154751_add_tag_id_to_memos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTagsTable extends Migration
+class AddTagIdToMemosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,8 @@ class CreateTagsTable extends Migration
      */
     public function up()
     {
-        Schema::create('tags', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name', 100);
-            $table->integer('user_id');
-            $table->timestamps();
+        Schema::table('memos', function (Blueprint $table) {
+            $table->bigInteger('tag_id')->nullable()->after('user_id');
         });
     }
 
@@ -28,6 +25,8 @@ class CreateTagsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tags');
+        Schema::table('memos', function (Blueprint $table) {
+            $table->dropColumn('tag_id');
+        });
     }
 }
